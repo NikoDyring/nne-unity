513ec6b290dde33d0894913fcd34a037a0dbb8c9
Nikolaj <Nikodyring@gmail.com>
2021-03-25 14:53:40 +0100

    Edited Readme.md and added Raycasting

---
+++ NNE Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField] float speed = 12f;
+    CharacterController controller;
+    RaycastHit hit;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<MeshRenderer>().material.color = Color.blue;
+    }
+
+    void Update()
+    {
+        if(!isLocalPlayer)
+        {
+            GetComponentInChildren<Camera>().enabled = false;
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 3f))
+            {
+                Debug.Log("I hit: " + hit.collider.gameObject);
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow, 2f);
+            }
+            else
+            {
+                Debug.Log("I missed");
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red, 2f);
+            }
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# nne-unity
-NNE Unity Assignment
+# NNE Unity Assignment
+
+By Nikolaj Dyring Jensen
+
+## Controls
+
+**WASD** - For moving your player.
+**E** - For interacting.
+
+## Considerations
+
+1. Since the testscene is very flat and has no stairs/slopes, I've decided not to implement gravity / stepping on the player controller. This would be easy to implement in the future if the needs were to come.
+
+## Other Comments
